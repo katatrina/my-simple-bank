8e83182671519af72e7c8bae3b6405ecac6cc1dd
chauvinhphuoc <cvphuoc2014@gmail.com>
2023-09-09 13:42:30 +0700

    mock DB for testing

--- Makefile
+++ Makefile
@@ -23,4 +23,7 @@ server:
 test:
 	go test -v -cover ./...
 
+mock:
+	mockgen -package mockdb -destination db/mock/store.go github.com/katatrina/my-simple-bank/db/sqlc Store
+
 .PHONY: postgres createdb dropdb migrateup migratedown sqlc test server
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -3,24 +3,32 @@ package db
 import (
 	"context"
 	"database/sql"
+
+	_ "github.com/golang/mock/mockgen/model"
 )
 
 // Store provides all functions to execute db queries and transactions.
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore Store provides all functions to execute SQL queries and transactions.
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 // NewStore creates a new Store.
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		Queries: New(db),
 		db:      db,
 	}
 }
 
 // execTx executes a generic database transaction.
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -60,7 +68,7 @@ type TransferTxResult struct {
 
 // TransferTx represents a money transfer transaction from one account to the other.
 // It provides instructions to execute the transaction: creates a transfer record, add account entries, and update accounts' balance.
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	// Keep in mind that qtx is a database transaction.
