703c40aa3e1dfc7663fdb6a8163e1c4a851ae03e
katatrina <62022374+katatrina@users.noreply.github.com>
2023-11-23 23:17:58 +0700

    load environment variable before running db migration (#6)

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "run db migrations"
+source /app/app.env
 /app/migrate -path /app/migrations -database "$DB_SOURCE" --verbose up # execute the "migrate" binary
 
 echo "start the app"
